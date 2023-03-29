ca861bf963ddee1beebca738f5acbbe6b13964b6
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-03-29 22:23:54 +0700

    remove fetch_data move everything to IOdb.py

--- fetch_data.py
+++
@@ -1,9 +0,0 @@
-import utils.connect
-
-
-def fetch_settings():
-    settings = utils.connect.get_settings()
-    for setting in settings:
-        print(setting)
-    print(settings)
-    return settings
