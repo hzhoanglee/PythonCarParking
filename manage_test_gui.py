065ab838844758459c9a0b8fe3a8bbec850d7c44
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-04-17 19:38:35 +0700

    fix test gui global variable gui

--- manage_test_gui.py
+++ manage_test_gui.py
@@ -430,7 +430,7 @@ class Builder:
                                        cursor="hand2",
                                        anchor='center',
                                        hover_color='#ccccff',
-                                       command=lambda: gui.open_check_in(),
+                                       command=lambda: self.gui.open_check_in(),
                                        ).place(x=0, y=250)
 
         # Manage vehicle
@@ -587,8 +587,8 @@ class Builder:
         self.ms.setup_parking_lot()
         self.build()
         self.my_time()
-        gui = ParkingBuildingGUI(self.ms)
-        gui.main_section()
+        self.gui = ParkingBuildingGUI(self.ms)
+        self.gui.main_section()
         self.root.mainloop()
 
 
